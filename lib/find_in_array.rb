5fc67c1c7762ebd270da313997f3a269bc9aaff4
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-22 15:36:36 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++
@@ -1,9 +0,0 @@
-def find_element_index(array, value_to_find)
-  length = array.length
-  length.times do |index|
-    if array[index] == value_to_find
-      return array.index(array[index])
-    else return nil
-    end
-  end
-end
